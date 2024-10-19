20a88bb0eb15ca5f42471908572e5a8137ce2f44
khalid-Nemo <khalid.laksantini@nemo.ma>
2024-10-19 19:15:09 +0100

    Add basic test for home route

---
+++ tests/test_app.py
@@ -0,0 +1,16 @@
+import pytest
+from api.routes import api_blueprint
+from flask import Flask
+
+@pytest.fixture
+def client():
+    app = Flask(__name__)
+    app.register_blueprint(api_blueprint)
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_home_route(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b"Observability" in response.data
