1e9891a6bc9d58614313c0ac4519e1fa303c22c7
khalid-Nemo <khalid.laksantini@nemo.ma>
2024-10-19 19:21:48 +0100

    Fix module import for tests and update CI/CD pipeline

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,7 +5,7 @@ from flask import Flask
 @pytest.fixture
 def client():
     app = Flask(__name__)
-    app.register_blueprint(api_blueprint)
+    app.register_blueprint(api_blueprint, url_prefix="/")
     app.config['TESTING'] = True
     with app.test_client() as client:
         yield client
